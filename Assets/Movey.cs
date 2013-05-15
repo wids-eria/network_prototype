02b1e0dc7972b391c78017e1d00640e391e38333
kalford <kalford@b9725fb4-600d-48a5-a689-d12b806c4d21>
2013-05-15 22:13:18 +0000

    Spawn cube script
    git-svn-id: svn+ssh://eria2/usr/share/subversion/repos/gls/network_prototype/trunk@596 b9725fb4-600d-48a5-a689-d12b806c4d21

--- Assets/Movey.cs
+++ Assets/Movey.cs
@@ -3,24 +3,14 @@ using System.Collections;
 
 public class Movey : MonoBehaviour {
 
-  // Use this for initialization
-  void Start () {
-
-  }
 
   // Update is called once per frame
   void Update () {
-    if (Input.GetKey(KeyCode.W)) {
-      //transform.RotateAround(transform.up, sweetshit * Time.deltaTime);
-      rigidbody.AddRelativeTorque (Vector3.forward * 60);
-    }
+    rigidbody.AddRelativeTorque (Vector3.forward * 60 * Input.GetAxis("Vertical")  );
+    rigidbody.AddTorque         (Vector3.up      * 30 * Input.GetAxis("Horizontal"));
 
-    if (Input.GetKey(KeyCode.Q)) {
-      rigidbody.AddTorque (Vector3.up * -30);
-    }
-
-    if (Input.GetKey(KeyCode.E)) {
-      rigidbody.AddTorque (Vector3.up * 30);
+    if(Input.GetButtonDown("Jump")) {
+      rigidbody.AddForce (Vector3.up * 400);
     }
   }
 }
--- Assets/Spawny.cs
+++ Assets/Spawny.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 using System.Collections;
 
 public class Spawny : MonoBehaviour {
+  public GameObject prefabulous;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+  // Update is called once per frame
+  void Update () {
+    if(Input.GetButtonDown("Fire1")) {
+      Instantiate (prefabulous, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+  }
 }
