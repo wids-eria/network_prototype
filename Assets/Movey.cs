a61b9cced296d28f1a162ea41f7c45b55476362a
kalford <kalford@b9725fb4-600d-48a5-a689-d12b806c4d21>
2013-05-15 22:35:47 +0000

    Default editable values for cube mover
    git-svn-id: svn+ssh://eria2/usr/share/subversion/repos/gls/network_prototype/trunk@598 b9725fb4-600d-48a5-a689-d12b806c4d21

--- Assets/Movey.cs
+++ Assets/Movey.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 using System.Collections;
 
 public class Movey : MonoBehaviour {
+  public int forward_speed = 60;
+  public int turning_force = 30;
+  public int jumping_force = 400;
 
 
   // Update is called once per frame
   void Update () {
-    rigidbody.AddRelativeTorque (Vector3.forward * 60 * Input.GetAxis("Vertical")  );
-    rigidbody.AddTorque         (Vector3.up      * 30 * Input.GetAxis("Horizontal"));
+    rigidbody.AddRelativeTorque (Vector3.forward * forward_speed * Input.GetAxis("Vertical")  );
+    rigidbody.AddTorque         (Vector3.up      * turning_force * Input.GetAxis("Horizontal"));
 
     if(Input.GetButtonDown("Jump")) {
-      rigidbody.AddForce (Vector3.up * 400);
+      rigidbody.AddForce (Vector3.up * jumping_force);
     }
   }
 }
