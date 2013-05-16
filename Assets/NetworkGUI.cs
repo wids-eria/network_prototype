f54f8d430d7a6817505314d82b8c4a75f13360c4
kalford <kalford@b9725fb4-600d-48a5-a689-d12b806c4d21>
2013-05-16 19:20:04 +0000

    Editable text area
    git-svn-id: svn+ssh://eria2/usr/share/subversion/repos/gls/network_prototype/trunk@629 b9725fb4-600d-48a5-a689-d12b806c4d21

--- Assets/NetworkGUI.cs
+++ Assets/NetworkGUI.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 using System.Collections;
 
 public class NetworkGUI : MonoBehaviour {
+  public string connect_host = "127.0.0.1"; // Store in player prefs
+
   public GUIStyle guiPixelBoxStyle;
   public GUIStyle guiDropShadowStyle;
   public GUIStyle guiMineBoxStyle;
 
+
   void OnGUI () {
     GUILayout.BeginArea (new Rect (10, 10, 200, 150), guiPixelBoxStyle);
 
-      GUILayout.Button    ("Host",      guiMineBoxStyle   );
-      GUILayout.Button    ("Connect",   guiMineBoxStyle   );
-      GUILayout.TextField ("127.0.0.1", guiDropShadowStyle);
+      if(GUILayout.Button ("Host",      guiMineBoxStyle)) { Debug.Log("host click");    }
+      if(GUILayout.Button ("Connect",   guiMineBoxStyle)) { Debug.Log("connect click"); }
+      connect_host = GUILayout.TextField (connect_host, guiDropShadowStyle);
 
     GUILayout.EndArea ();
   }
