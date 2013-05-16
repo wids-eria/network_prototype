3168fb809211e793d684eadb57881b22005bb0be
kalford <kalford@b9725fb4-600d-48a5-a689-d12b806c4d21>
2013-05-16 16:26:55 +0000

    Pixel border gui style
    git-svn-id: svn+ssh://eria2/usr/share/subversion/repos/gls/network_prototype/trunk@615 b9725fb4-600d-48a5-a689-d12b806c4d21

--- Assets/NetworkGUI.cs
+++ Assets/NetworkGUI.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 using System.Collections;
 
 public class NetworkGUI : MonoBehaviour {
+  public GUIStyle guiPixelBoxStyle;
+
   void OnGUI () {
-    GUILayout.BeginArea (new Rect (10, 10, 100, 100));
+    GUILayout.BeginArea (new Rect (10, 10, 100, 100), guiPixelBoxStyle);
 
       GUILayout.Button ("Client");
       GUILayout.Button ("Server");
