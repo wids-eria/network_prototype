fe3ee700f8ccbb22977d381cc095ad6ffd208751
kalford <kalford@b9725fb4-600d-48a5-a689-d12b806c4d21>
2013-05-16 00:53:46 +0000

    Random colors, and material by name
    git-svn-id: svn+ssh://eria2/usr/share/subversion/repos/gls/network_prototype/trunk@599 b9725fb4-600d-48a5-a689-d12b806c4d21

--- Assets/Spawny.cs
+++ Assets/Spawny.cs
@@ -3,12 +3,23 @@ using System.Collections;
 
 public class Spawny : MonoBehaviour {
   public GameObject prefabulous;
+  public GameObject spawn_point;
 
 
   // Update is called once per frame
   void Update () {
     if(Input.GetButtonDown("Fire1")) {
-      Instantiate (prefabulous, new Vector3(0, 0, 0), Quaternion.identity);
+      GameObject new_thing = (GameObject)Instantiate (prefabulous, spawn_point.transform.position, Quaternion.identity);
+
+      if (Random.value > 0.5) {
+        new_thing.renderer.material.SetColor("_Color", new Color(Random.value, Random.value, Random.value));
+      }
+      else if (Random.value > 0.5F) {
+        Material thing2 = (Material)Instantiate(Resources.Load("Thing2"));
+        new_thing.renderer.material = thing2;
+        new_thing.transform.localScale += new Vector3(0.5F,0.5F,0.5F);
+
+      }
     }
   }
 }
