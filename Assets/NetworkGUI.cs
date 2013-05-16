738fda7d707c56660c39ee31975353deea51e864
kalford <kalford@b9725fb4-600d-48a5-a689-d12b806c4d21>
2013-05-16 17:31:40 +0000

    Networking gui complete
    git-svn-id: svn+ssh://eria2/usr/share/subversion/repos/gls/network_prototype/trunk@617 b9725fb4-600d-48a5-a689-d12b806c4d21

--- Assets/NetworkGUI.cs
+++ Assets/NetworkGUI.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class NetworkGUI : MonoBehaviour {
   public GUIStyle guiPixelBoxStyle;
   public GUIStyle guiDropShadowStyle;
+  public GUIStyle guiMineBoxStyle;
 
   void OnGUI () {
     GUILayout.BeginArea (new Rect (10, 10, 200, 150), guiPixelBoxStyle);
 
-      GUILayout.Button ("Host");
-      GUILayout.Button ("Connect");
-      GUILayout.TextField("127.0.0.1", guiDropShadowStyle);
+      GUILayout.Button    ("Host",      guiMineBoxStyle   );
+      GUILayout.Button    ("Connect",   guiMineBoxStyle   );
+      GUILayout.TextField ("127.0.0.1", guiDropShadowStyle);
 
     GUILayout.EndArea ();
   }
