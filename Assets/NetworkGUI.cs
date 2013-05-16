59b9cb1279a523ed46617ec0166fce3859393df0
kalford <kalford@b9725fb4-600d-48a5-a689-d12b806c4d21>
2013-05-16 16:44:23 +0000

    Text input dropshadow
    git-svn-id: svn+ssh://eria2/usr/share/subversion/repos/gls/network_prototype/trunk@616 b9725fb4-600d-48a5-a689-d12b806c4d21

--- Assets/NetworkGUI.cs
+++ Assets/NetworkGUI.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class NetworkGUI : MonoBehaviour {
   public GUIStyle guiPixelBoxStyle;
+  public GUIStyle guiDropShadowStyle;
 
   void OnGUI () {
-    GUILayout.BeginArea (new Rect (10, 10, 100, 100), guiPixelBoxStyle);
+    GUILayout.BeginArea (new Rect (10, 10, 200, 150), guiPixelBoxStyle);
 
-      GUILayout.Button ("Client");
-      GUILayout.Button ("Server");
+      GUILayout.Button ("Host");
+      GUILayout.Button ("Connect");
+      GUILayout.TextField("127.0.0.1", guiDropShadowStyle);
 
     GUILayout.EndArea ();
   }
